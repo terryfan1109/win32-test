811fbd63871a5791a2c586f990c2e6cb80678026
Terry Fan <terry_fan@terrys-mbp.htctaipei.htc.com.tw>
2015-09-02 14:37:27 +0800

    UPD. test connection point

--- EchoClient/EchoClient.cpp
+++ EchoClient/EchoClient.cpp
@@ -26,17 +26,51 @@ public:
 	}
 };
 
+_ATL_FUNC_INFO DISP_ON_EVENT_INFO =
+{
+	CC_STDCALL,                                 // Calling convention.
+	VT_EMPTY,                                   // Return type.
+	1,                                          // Number of arguments.
+	{ VT_I4 }									// VariantArgument types.
+};
+
+class ProxyEventSink :
+	public IDispEventImpl<0, ProxyEventSink, &__uuidof(libCSharpEcho::CalculatorEvent)>
+{
+public:
+	ProxyEventSink() {}
+
+	BEGIN_SINK_MAP(ProxyEventSink)
+		SINK_ENTRY_INFO(0,											// ID of event source
+					    __uuidof(libCSharpEcho::CalculatorEvent),   // interface to listen to
+					    0x1,										// dispatch ID to receive
+					    onEvent,									// method to call when event arrives
+					    &DISP_ON_EVENT_INFO)						// parameter info for method call
+	END_SINK_MAP()
+
+private:
+	void __stdcall onEvent(long result) {
+		wprintf(L"onEvent %d\r\n", result);
+	}
+};
+
 
 static HRESULT __stdcall MyCalculateCallback(long value) {
-	wprintf(L"...%d", value);
+	wprintf(L"...%d\r\n", value);
 	return S_OK;
 }
 
 void process() {
+	HRESULT hr = E_FAIL;
+
 	libCSharpEcho::CalculatorPtr calculator(__uuidof(libCSharpEcho::CalculatorImpl));
-	_bstr_t input = _bstr_t(L"hello!");
+
+	ProxyEventSink eventSink;
+	hr = eventSink.DispEventAdvise(calculator);
+
+	_bstr_t input = _bstr_t(L"Hello");
 	int result = calculator->calculate(input);
-	wprintf(L"...%d", result);
+	wprintf(L"...%d\r\n", result);
 
 	CComObject<ProxyCallback>* pv;
 	CComObject<ProxyCallback>::CreateInstance(&pv);
@@ -44,6 +78,8 @@ void process() {
 	pCallback.Attach(pv, false);
 
 	calculator->asyncCalculate(pCallback, input);
+
+	hr = eventSink.DispEventUnadvise(calculator, &eventSink.m_iid);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
--- libCSharpEcho/EchoImpl.cs
+++ libCSharpEcho/EchoImpl.cs
@@ -7,14 +7,21 @@ using System.Runtime.InteropServices;
 
 namespace libCSharpEcho
 {
-    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("D94F4C9B-5C08-4566-B3DB-A3B6C543F485")]
-    public interface CalculatorCallback
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate void eventDelegate(int result);
+
+    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsIDispatch), Guid("CCF7FD24-6395-4885-9798-5E125587AD8F")]
+    public interface CalculatorEvent 
     {
-        void callback(int result);
+        [DispId(1)]
+        void onEvent(int result);
     }
 
-    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-    public delegate void onEvent(int result);
+    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("D94F4C9B-5C08-4566-B3DB-A3B6C543F485")]
+    public interface CalculatorCallback
+    {   
+        void callback(int result);
+    }
 
     [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsDual), Guid("A6193A2C-65F4-430D-A4EB-143D6B9A0EE4")]
     public interface Calculator
@@ -24,15 +31,28 @@ namespace libCSharpEcho
     }
 
     [ComVisible(true), ClassInterface(ClassInterfaceType.None), Guid("A77CB584-8C98-4463-B090-84418F1740AF")]
+    [ComSourceInterfaces(typeof(CalculatorEvent))]
     public class CalculatorImpl : Calculator
     {
+        public event eventDelegate onEvent;
+
         public int calculate(String value)
         {
+            if (null != onEvent)
+            {
+                onEvent(1);
+            }
+
             return value.Length;
         }
 
         public void asyncCalculate(CalculatorCallback callback, String value)
         {
+            if (null != onEvent)
+            {
+                onEvent(2);
+            }
+
             callback.callback(value.Length);
         }
     }
